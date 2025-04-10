fbb25230619124d31307cedb9117bd6dc3157f41
Speedy_Lex <78314533+speedy-lex@users.noreply.github.com>
2025-04-10 23:48:00 +0200

    add skill icons

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
-with open("template.md") as r:
+def replace(string, dictionary):
+    for (k, v) in dictionary.items():
+        string = string.replace(f"$({k})", v)
+    return string
+
+def get_skills_html(skills):
+    base_url = "https://go-skill-icons.vercel.app/api/icons?i="
+    skills_str = ",".join(sorted(skills))
+    img_url = f"{base_url}{skills_str}&perline=8"
+
+    return f"""
+<p align="center">
+    <a href="https://github.com/LelouchFR/skill-icons">
+        <img src="{img_url}" />
+    </a>
+</p>
+    """
+
+with open("template.md", encoding="utf-8") as r:
     template = r.read()
 
-with open("README.md", "w") as w:
+replacements = {
+    "skills": get_skills_html(["assembly", "arch", "codeberg", "firefox", "arduino", "bash", "blender", "c", "css", "desmos", "flask", "github", "html", "hyprland", "iced", "inkscape", "linux", "markdown", "python", "rust", "raspberrypi", "vscode", "virtualbox", "tmux"])
+}
+
+template = replace(template, replacements)
+
+with open("README.md", "w", encoding="utf-8") as w:
     w.write(template)
--- template.md
+++ template.md
@@ -1,6 +1,11 @@
 # Hi, I'm Speedy_Lex 👋
+
+## About Me
 - I develop primarily in Rust 🔨
 - I live in the Netherlands 🇳🇱
 - I make AI, but from scratch 🤖
 - I like math ➗
 - Talk to me on discord ✉️: [@speedy_lex](https://discord.com/users/824339532996411432)
+
+## Skills
+$(skills)
